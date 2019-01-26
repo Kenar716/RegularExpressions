581a51023b2d712bd3f39031cba6904c3c6657cc
Carlos Valdez <31641682+Kenar716@users.noreply.github.com>
2019-01-26 04:01:30 -0600

    Se agrega el codigo para verificar si la linea de texto del archivo cumple con la expresion regular.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace RegularExpressionesNetCore
 {
@@ -6,8 +7,30 @@ namespace RegularExpressionesNetCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            Console.WriteLine("Proceso iniciado.");
+            
+            System.IO.StreamReader file = new System.IO.StreamReader(
+                @"C:\Users\cvald\PersonalGit\Platzi\Curso Expresiones Regulares\Proyecto\RegularExpressionesNetCore\results games.csv");
+
+            string line;
+            int totalLines = 0;
+            int totalMatches = 0;
+            //Regex regex = new Regex(@"Mexico");
+            // Regex regex = new Regex(@"^2011\-.*$"); //AllGamesOnYear2011
+            Regex regex = new Regex(@"^2011\-.*[zk].*$"); //AllGamesOnYear2011WithLetterZorK
+            while ((line = file.ReadLine()) != null)
+            {
+                Match match = regex.Match(line);
+                if (match.Success)
+                {
+                    Console.WriteLine(line);
+                    totalMatches++;
+                }
+                totalLines ++;
+            }
+            Console.WriteLine($"Total lines: {totalLines}");
+            Console.WriteLine($"Total matches: {totalMatches}");
+            Console.WriteLine("Proceso terminado.");
         }
     }
 }
